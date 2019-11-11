e5b4488bf0c90d92a7ed4119f17cfae7c8dfe47e
Dale Dybvig <dalerdybvig@gmail.com>
2019-11-11 12:43:52 -0600

    initial commit

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "poo";
+            string b = "pee";
+            Console.WriteLine(a + a + b + b);
+            Console.ReadLine();
+        }
+    }
+}
