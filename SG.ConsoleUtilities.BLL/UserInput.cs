a4ab53f417f69ab630df03266cd3b2c3984a4317
Dale Dybvig <dalerdybvig@gmail.com>
2019-11-25 13:47:41 -0600

    starting on some console utilities for c# stuff.

---
+++ SG.ConsoleUtilities.BLL/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SG.ConsoleUtilities.BLL
+{
+    public class UserInput
+    {
+        public static int GetIntFromUser(string prompt)
+        {
+            int result;
+            string userInput;
+
+            while(true)
+            {
+                Console.Write(prompt);
+                userInput = Console.ReadLine();
+
+                if(int.TryParse(userInput, out result))
+                {
+                    return result;
+                }
+
+                Console.WriteLine("That is not a valid input.");
+            }
+        }
+    }
+}
